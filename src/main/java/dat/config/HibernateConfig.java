0076ec8932048f1596487ad09ff5471c685f0039
Jonathan0912000 <Jonathan0912@gmail.com>
2025-10-07 15:22:48 +0200

    Correctly update HibernateConfig to use env vars

--- src/main/java/dat/config/HibernateConfig.java
+++ src/main/java/dat/config/HibernateConfig.java
@@ -4,7 +4,6 @@ import dat.entities.Hotel;
 import dat.entities.Room;
 import dat.security.entities.Role;
 import dat.security.entities.User;
-import dat.utils.Utils;
 import jakarta.persistence.EntityManagerFactory;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
@@ -16,105 +15,72 @@ import java.util.Properties;
 public class HibernateConfig {
 
     private static EntityManagerFactory emf;
-    private static EntityManagerFactory emfTest;
-    private static boolean isTest = false; // RETTET: Fra Boolean til boolean
-
-    // RETTET: Fra Boolean til boolean
-    public static void setTest(boolean test) {
-        isTest = test;
-    }
-
-    // RETTET: Fra Boolean til boolean
-    public static boolean isTest() {
-        return isTest;
-    }
+    private static boolean isTest = false; // Simpel boolean, da den kun er true/false
 
     public static EntityManagerFactory getEntityManagerFactory() {
-        if (emf == null)
-            emf = createEMF(isTest()); // Bruger isTest() direkte
+        if (emf == null) {
+            emf = createEMF();
+        }
         return emf;
     }
 
     public static EntityManagerFactory getEntityManagerFactoryForTest() {
-        if (emfTest == null){
-            setTest(true); // Denne virker nu, fordi setTest forventer en boolean
-            emfTest = createEMF(isTest());
+        isTest = true;
+        if (emf == null) {
+            emf = createEMF();
         }
-        return emfTest;
+        return emf;
     }
 
-    // TODO: IMPORTANT: Add Entity classes here for them to be registered with Hibernate
-    private static void getAnnotationConfiguration(Configuration configuration) {
-        configuration.addAnnotatedClass(Hotel.class);
-        configuration.addAnnotatedClass(Room.class);
-        configuration.addAnnotatedClass(User.class);
-        configuration.addAnnotatedClass(Role.class);
-    }
-
-    private static EntityManagerFactory createEMF(boolean forTest) {
+    private static EntityManagerFactory createEMF() {
         try {
             Configuration configuration = new Configuration();
             Properties props = new Properties();
-            setBaseProperties(props);
-            if (forTest) {
-                props = setTestProperties(props);
+
+            // Fælles properties
+            props.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+            props.put("hibernate.connection.driver_class", "org.postgresql.Driver");
+            props.put("hibernate.current_session_context_class", "thread");
+            props.put("hibernate.show_sql", "true");
+            props.put("hibernate.format_sql", "true");
+            props.put("hibernate.use_sql_comments", "true");
+
+            // Håndtering af forskellige miljøer
+            if (isTest) {
+                // Test-database opsætning
+                props.put("hibernate.connection.driver_class", "org.testcontainers.jdbc.ContainerDatabaseDriver");
+                props.put("hibernate.connection.url", "jdbc:tc:postgresql:15.3-alpine3.18:///test_db");
+                props.put("hibernate.hbm2ddl.auto", "create-drop");
             } else if (System.getenv("DEPLOYED") != null) {
-                setDeployedProperties(props);
+                // Server (DigitalOcean) opsætning
+                props.setProperty("hibernate.connection.url", System.getenv("CONNECTION_STR"));
+                props.setProperty("hibernate.connection.username", System.getenv("DB_USERNAME"));
+                props.setProperty("hibernate.connection.password", System.getenv("DB_PASSWORD"));
+                props.put("hibernate.hbm2ddl.auto", "update");
             } else {
-                props = setDevProperties(props);
+                // Lokal udviklings-opsætning
+                props.put("hibernate.connection.url", "jdbc:postgresql://localhost:5432/hotel");
+                props.put("hibernate.connection.username", "postgres");
+                props.put("hibernate.connection.password", "postgres");
+                props.put("hibernate.hbm2ddl.auto", "update");
             }
+
             configuration.setProperties(props);
-            getAnnotationConfiguration(configuration);
+
+            // Tilføj dine entity-klasser
+            configuration.addAnnotatedClass(Hotel.class);
+            configuration.addAnnotatedClass(Room.class);
+            configuration.addAnnotatedClass(User.class);
+            configuration.addAnnotatedClass(Role.class);
 
             ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
                     .applySettings(configuration.getProperties())
                     .build();
             SessionFactory sf = configuration.buildSessionFactory(serviceRegistry);
-            return sf.unwrap(EntityManagerFactory.class); // Simpelt return
-        }
-        catch (Throwable ex) {
+            return sf.unwrap(EntityManagerFactory.class);
+        } catch (Throwable ex) {
             System.err.println("Initial SessionFactory creation failed." + ex);
             throw new ExceptionInInitializerError(ex);
         }
-    }
-
-    private static Properties setBaseProperties(Properties props) {
-        props.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
-        props.put("hibernate.connection.driver_class", "org.postgresql.Driver");
-        props.put("hibernate.hbm2ddl.auto", "update"); // Ændret til 'update' for at undgå at slette data ved hver start
-        props.put("hibernate.current_session_context_class", "thread");
-        props.put("hibernate.show_sql", "true");
-        props.put("hibernate.format_sql", "true");
-        props.put("hibernate.use_sql_comments", "true");
-        return props;
-    }
-
-    private static Properties setDeployedProperties(Properties props) {
-        // Bruger de variabler du allerede har defineret i din .env fil
-        props.setProperty("hibernate.connection.url", System.getenv("CONNECTION_STR"));
-        props.setProperty("hibernate.connection.username", System.getenv("DB_USERNAME"));
-        props.setProperty("hibernate.connection.password", System.getenv("DB_PASSWORD"));
-        return props;
-    }
-
-    private static Properties setDevProperties(Properties props) {
-        // Denne del vil nu give en fejl, fordi den leder efter 'config.properties', som ikke er en del af server-setup'et.
-        // Men det er OK, fordi på serveren vil setDeployedProperties blive kaldt i stedet.
-        String DBName = Utils.getPropertyValue("DB_NAME", "config.properties");
-        props.put("hibernate.connection.url", "jdbc:postgresql://localhost:5432/" + DBName);
-        props.put("hibernate.connection.username", "postgres");
-        props.put("hibernate.connection.password", "postgres");
-        return props;
-    }
-
-    private static Properties setTestProperties(Properties props) {
-        props.put("hibernate.connection.driver_class", "org.testcontainers.jdbc.ContainerDatabaseDriver");
-        props.put("hibernate.connection.url", "jdbc:tc:postgresql:15.3-alpine3.18:///test_db");
-        props.put("hibernate.connection.username", "postgres");
-        props.put("hibernate.connection.password", "postgres");
-        props.put("hibernate.archive.autodetection", "class");
-        props.put("hibernate.show_sql", "true");
-        props.put("hibernate.hbm2ddl.auto", "create-drop");
-        return props;
     }
 }
